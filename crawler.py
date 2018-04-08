85185ff8bc8d3c2bd878076dce9d5602ea5cbf4a
Marat Zaynutdinov <tsundokum@gmail.com>
2018-04-08 20:59:18 +0300

    add crawler

---
+++ crawler.py
@@ -0,0 +1,42 @@
+import scrapy
+
+NKVD_MEMO_ENTRY = 'https://nkvd.memo.ru/index.php/%D0%9D%D0%9A%D0%92%D0%94:%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0'
+
+ROOT = 'https://nkvd.memo.ru'
+
+
+class NKVDMemo(scrapy.Spider):
+    name = "nkvdmemo"
+
+    def start_requests(self):
+        yield scrapy.Request(url=NKVD_MEMO_ENTRY, callback=self.parse_letters)
+
+    def parse_letters(self, response):
+        letters = response.xpath("//a[starts-with(@title, 'НКВД:Именной указатель:')]/@href").extract()
+        for let in letters:
+            yield response.follow(ROOT + let, self.parse_names)
+
+    def parse_names(self, response):
+        names = response.xpath("//div[@class='mw-parser-output']//a[not(starts-with(@title,'НКВД'))]/@href").extract()
+        for n in names:
+            yield response.follow(ROOT + n, self.parse_destination_table)
+
+    def parse_destination_table(self, response):
+
+        name = response.xpath("//h1[@class='firstHeading']/text()").extract_first()
+
+        table = response.xpath("//table[@class='nkvd-designation-table']")
+
+        result = []
+        for row in table:
+            marker = row.xpath(".//td[@class='nkvd-table-marker']/span/@title").extract_first()
+            prep = row.xpath(".//td[@class='nkvd-table-preposition']//text()").extract_first()
+            date = row.xpath(".//td[@class='nkvd-table-date']//text()").extract_first()
+            pos = row.xpath(".//td[@class='nkvd-table-position']//text()").extract_first()
+            source = row.xpath(".//td[@class='nkvd-table-source']//text()").extract_first()
+            unit = row.xpath(".//td[@class='nkvd-table-unit']//text()").extract_first()
+            result.append([marker, prep, date, pos, source, unit])
+
+        yield {"link": response.request.url,
+               "name": name,
+               "table": result}
