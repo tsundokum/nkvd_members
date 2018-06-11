fa9e1900e151f1f7f7e690d59eabaae8580cd247
tsundokum <tsundokum@gmail.com>
2018-06-11 19:57:38 +0300

    crawler: bug fix + crawl rank and bio text

--- crawler.py
+++ crawler.py
@@ -22,21 +22,40 @@ class NKVDMemo(scrapy.Spider):
             yield response.follow(ROOT + n, self.parse_destination_table)
 
     def parse_destination_table(self, response):
-
         name = response.xpath("//h1[@class='firstHeading']/text()").extract_first()
+        bio = ''.join(response.xpath("//p[@class='nkvd-bio']//text()").extract())
+        party = ''.join(response.xpath("//p[@class='nkvd-party']//text()").extract())
+        nkvd = ''.join(response.xpath("//p[@class='nkvd-nkvd']//text()").extract())
+        repres = ''.join(response.xpath("//p[@class='nkvd-repr']//text()").extract())
+        info = ''.join(response.xpath("//p[@class='nkvd-info']//text()").extract())
 
-        table = response.xpath("//table[@class='nkvd-designation-table']")
-
-        result = []
-        for row in table:
+        designation_table = response.xpath("//table[@class='nkvd-designation-table']/tr[starts-with(@id, 'mw-customcollapsible-designation')]")
+        designations = []
+        for row in designation_table:
             marker = row.xpath(".//td[@class='nkvd-table-marker']/span/@title").extract_first()
             prep = row.xpath(".//td[@class='nkvd-table-preposition']//text()").extract_first()
             date = row.xpath(".//td[@class='nkvd-table-date']//text()").extract_first()
             pos = row.xpath(".//td[@class='nkvd-table-position']//text()").extract_first()
             source = row.xpath(".//td[@class='nkvd-table-source']//text()").extract_first()
             unit = row.xpath(".//td[@class='nkvd-table-unit']//text()").extract_first()
-            result.append([marker, prep, date, pos, source, unit])
+            designations.append([marker, prep, date, pos, source, unit])
+
+        rank_table = response.xpath("//table[@class='nkvd-rank-table']/tr[starts-with(@id, 'mw-customcollapsible-rank')]")
+        ranks = []
+        for row in rank_table :
+            marker = row.xpath(".//td[@class='nkvd-table-marker']/span/@title").extract_first()
+            prep = row.xpath(".//td[@class='nkvd-table-preposition']//text()").extract_first()
+            date = row.xpath(".//td[@class='nkvd-table-date']//text()").extract_first()
+            rank = row.xpath(".//td[@class='nkvd-table-rank']//text()").extract_first()
+            source = row.xpath(".//td[@class='nkvd-table-source']//text()").extract_first()
+            ranks.append([marker, prep, date, rank, source])
 
         yield {"link": response.request.url,
                "name": name,
-               "table": result}
+               "bio": bio,
+               "party": party,
+               "nkvd": nkvd,
+               "repr": repres,
+               "info": info,
+               "designations": designations,
+               "ranks": ranks}
